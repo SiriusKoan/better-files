3450b9461cb62c4715fc25547b346dab31c6feeb
SiriusKoan <k.peihsun@gmail.com>
2022-08-31 22:10:42 +0800

    Add css and import font from js

---
+++ font.js
@@ -0,0 +1,5 @@
+var link = document.createElement('link');
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('type', 'text/css');
+link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+document.head.appendChild(link);
